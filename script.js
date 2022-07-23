d89031ac65c74a4f5f5a6f7178b1896c848fe8c3
Cole <propertyisrobbery@hotmail.com>
2022-07-23 17:26:07 -0400

    draft playerselection feature

--- script.js
+++ script.js
@@ -2,6 +2,27 @@
 
 const gameboard = (() => {
   const board = [];
+  //makes an array with 9 empty spaces:
+  for (let i = 0; i < 9; i++) {
+    board.push('');
+  }
 
-  const player = function () {};
+  let squares = document.querySelectorAll('.grid');
+  squares.forEach((square) => {});
+  //not sure what will be returned or how they returned multiple things, check module info
+  return board;
 })();
+const player = (function () {
+  let xMarker = document.querySelector('#X');
+  let oMarker = document.querySelector('#O');
+  let playerMarker = xMarker;
+  xMarker.addEventListener('click', () => {
+    playerMarker = xMarker;
+  });
+  oMarker.addEventListener('click', () => {
+    playerMarker = oMarker;
+  });
+  return playerMarker;
+})();
+console.log(playerMarker);
+// need index of array and on click change value of array and innertext of grid square to playermarker.id
