973c370a55154603efb0be35df2b67294840d6da
Cole <propertyisrobbery@hotmail.com>
2022-07-27 17:49:37 -0400

    attempt wincons and function to mark board

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ const gameboard = (() => {
   //makes an array with 9 empty spaces:
   for (let i = 0; i < 9; i++) {
     board.push('');
+    console.log(board);
   }
-
   let squares = document.querySelectorAll('.grid');
-  squares.forEach((square) => {});
+  squares.forEach((square) => {
+    document.addEventListener('click', markBoard);
+  });
+  //this is supposed to take the square they are clicking and set it to either X or O - needs to be linked to board array?
+  const markBoard = function (playerMarker) {
+    this.target.textContent = `${playerMarker}`;
+  };
   //not sure what will be returned or how they returned multiple things, check module info
   return board;
 })();
@@ -24,5 +30,21 @@ const player = (function () {
   });
   return playerMarker;
 })();
-console.log(playerMarker);
+console.log(player.playerMarker);
 // need index of array and on click change value of array and innertext of grid square to playermarker.id
+
+//not sure if this is the correct way to store it or how to use this yet
+const winCon = [
+  //horizontal
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  //vertical
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  //diagonal
+  [0, 4, 8],
+  [2, 4, 6],
+];
+// view (updating board display), game (keeping track of which grid squares are full, turn, and win con), player(keeping track of X or O?)
