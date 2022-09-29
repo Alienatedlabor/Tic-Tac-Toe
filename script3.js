57535199b9648af4f93c2f94be914253f18a484e
Cole <propertyisrobbery@hotmail.com>
2022-09-28 20:22:07 -0400

    create array, create markBoard function

---
+++ script3.js
@@ -0,0 +1,41 @@
+//gameboard module//
+
+const GAMEBOARD = (() => {
+  let cellList = document.querySelectorAll('.grid');
+  let boardArray = Array.from(cellList);
+  console.log(boardArray);
+
+  const player = (function () {
+    //won't switch turns, not sure about equivalencies, doesn't export playermarker the way I intended.
+
+    function getPlayerTurn() {
+      let playerMarker;
+      let XTURN = true;
+
+      if ((XTURN = true)) {
+        playerMarker = 'X';
+        return playerMarker;
+      } else XTURN = false;
+
+      if ((XTURN = false)) {
+        playerMarker = 'O';
+        return playerMarker;
+      }
+      return playerMarker;
+    }
+    return getPlayerTurn;
+  })();
+
+  for (let i = 0; i < boardArray.length; i++) {
+    boardArray[i].addEventListener('click', player.getPlayerTurn);
+    boardArray[i].addEventListener('click', markBoard, { once: true });
+    // maybe a turn switch event which just checks whether xturn is true or false and negates it
+  }
+
+  function markBoard(playerMarker) {
+    boardArray[
+      this.dataset.index
+    ].textContent = `${player.getPlayerTurn.playerMarker}`;
+    console.log(playerMarker);
+  }
+})();
